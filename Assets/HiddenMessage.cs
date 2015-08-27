671373088df11d3dd9703ae8ff2d5680eebf2395
Reecey95 <reecehall_rc@blueyonder.co.uk>
2015-08-27 09:50:49 +0100

    Added HiddenMessage

---
+++ Assets/HiddenMessage.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HiddenMessage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("You found the message, you must be the hero destined to save the minions from their destruction");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
