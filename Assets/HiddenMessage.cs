b5c93712b9ffd4d11cc32205b6156a643aa797e0
Reecey95 <reecehall_rc@blueyonder.co.uk>
2015-08-27 09:51:53 +0100

    Updated Message

--- Assets/HiddenMessage.cs
+++ Assets/HiddenMessage.cs
@@ -6,6 +6,7 @@ public class HiddenMessage : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		print("You found the message, you must be the hero destined to save the minions from their destruction");
+		print ("Gru is going to go crazy as there is no bananas left, help them!");
 	}
 	
 	// Update is called once per frame
